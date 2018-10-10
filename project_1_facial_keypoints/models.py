61bb50474cfa74f32e6bae7743f78046ae0b6f62
Bjarten <bms_mok@hotmail.com>
2018-10-10 10:37:40 +0200

    feat: add LeNet to models

--- project_1_facial_keypoints/models.py
+++ project_1_facial_keypoints/models.py
@@ -95,3 +95,53 @@ class NamishNet(nn.Module):
                 pretty_net_str += f'\n{items}'
             pretty_net_str += '\n'
         return pretty_net_str
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet_1, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            OrderedDict([
+            ('conv1', nn.Conv2d(1, 64, kernel_size=5, padding = 2)),
+            ('relu1', nn.ReLU())]))
+
+        self.maxp1 = nn.Sequential(OrderedDict([
+            ('maxp1', nn.MaxPool2d(2, 2))]))
+
+        self.conv2 = nn.Sequential(OrderedDict([
+            ('conv2', nn.Conv2d(64, 128, kernel_size=5, padding = 2)),
+            ('relu2', nn.ReLU())]))
+
+        self.maxp2 = nn.Sequential(OrderedDict([
+            ('maxp2', nn.MaxPool2d(2, 2))]))
+
+        self.fc1 = nn.Sequential(OrderedDict([
+            ('fc1', nn.Linear(128 *  56 *  56, 544)),
+            ('relu3', nn.ReLU())]))
+
+        self.fc2 = nn.Sequential(OrderedDict([
+            ('fc2', nn.Linear(544, 272)),
+            ('relu4', nn.ReLU())]))
+
+        self.fc3 = nn.Sequential(OrderedDict([
+            ('fc3', nn.Linear(272, 136))]))
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.maxp1(out)
+        out = self.conv2(out)
+        out = self.maxp2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+
+    def __str__(self):
+        pretty_net_str = ''
+        for layer_name in self._modules:
+            pretty_net_str += f'{layer_name}:'
+            for items in getattr(self, layer_name):
+                pretty_net_str += f'\n{items}'
+            pretty_net_str += '\n'
+        return pretty_net_str
