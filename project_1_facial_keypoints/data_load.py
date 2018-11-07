b0e440aede5718e9c65955017b3294e8f598be0c
Bjarten <bms_mok@hotmail.com>
2018-11-07 15:50:00 +0100

    feat: create EyebrowsKeypointsDataset class

--- project_1_facial_keypoints/data_load.py
+++ project_1_facial_keypoints/data_load.py
@@ -85,7 +85,46 @@ class OutlineKeypointsDataset(Dataset):
 
         return sample
     
+class EyebrowsKeypointsDataset(Dataset):
+    """Face Landmarks dataset."""
 
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.key_pts_frame = pd.read_csv(csv_file)
+        self.key_pts_frame = self.key_pts_frame.loc[:, ['Unnamed: 0','34','35','36','37','38','39',
+                                                        '40','41','42','43','44','45','46','47',
+                                                        '48','49','50','51','52','53']]
+        print(self.key_pts_frame)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.key_pts_frame)
+
+    def __getitem__(self, idx):
+        image_name = os.path.join(self.root_dir,
+                                self.key_pts_frame.iloc[idx, 0])
+        
+        image = mpimg.imread(image_name)
+        
+        # if image has an alpha color channel, get rid of it
+        if(image.shape[2] == 4):
+            image = image[:,:,0:3]
+        
+        key_pts = self.key_pts_frame.iloc[idx, 1:].values
+        key_pts = key_pts.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'keypoints': key_pts}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
 
     
 # tranforms
