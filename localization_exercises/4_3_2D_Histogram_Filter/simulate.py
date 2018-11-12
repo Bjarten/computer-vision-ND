eb89e689b7093f798864ec73313e20610a3190dc
Bjarten <bms_mok@hotmail.com>
2018-11-12 11:23:49 +0100

    fix: update print syntax to work with python 3

--- localization_exercises/4_3_2D_Histogram_Filter/simulate.py
+++ localization_exercises/4_3_2D_Histogram_Filter/simulate.py
@@ -43,7 +43,7 @@ class Simulation(object):
 		beliefs = deepcopy(self.beliefs)
 		new_beliefs = localizer.sense(color, self.grid, beliefs, self.p_hit, self.p_miss)
 		if not new_beliefs or len(new_beliefs) == 0:
-			print "NOTE! The robot doesn't have a working sense function at this point."
+			print("NOTE! The robot doesn't have a working sense function at this point.")
 			self.beliefs = beliefs
 		else:
 			self.beliefs = new_beliefs
