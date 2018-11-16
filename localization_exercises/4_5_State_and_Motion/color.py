6a704555b0a87849de1c08bdde9c861a3a57418a
Bjarten <bms_mok@hotmail.com>
2018-11-16 08:00:04 +0100

    feat: complete all functions in Color class

--- localization_exercises/4_5_State_and_Motion/color.py
+++ localization_exercises/4_5_State_and_Motion/color.py
@@ -14,21 +14,17 @@ class Color(object):
     def __init__(self, r, g, b):
         # Setting the r value
         self.r = r
-        
-        ## TODO: Set the other two color variables g and b
-        
-
+        self.g = g
+        self.b = b
+                
     # __repr__ is called when a color is printed using print(some_color)
     # It must return a string
     def __repr__(self):
         '''Display a color swatch and then return a text description of r,g,b values.'''
         
         plt.imshow([[(self.r/255, self.g/255, self.b/255)]])
-        
-        ## TODO: Write a string representation for the color
-        ## ex. "rgb = [self.r, self.g, self.b]"
-        ## Right now this returns an empty string
-        string = ''
+
+        string = f'rgb = [{self.r}, {self.g}, {self.b}]'
         
         return string
     
