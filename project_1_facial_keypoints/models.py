acd2933db44afc3e73f6f0ed01f98e834277a04a
Bjarten <bms_mok@hotmail.com>
2018-11-21 14:59:59 +0100

    feat(p1): add resnet18_grayscale

--- project_1_facial_keypoints/models.py
+++ project_1_facial_keypoints/models.py
@@ -389,3 +389,16 @@ class resnet18(nn.Module):
     def forward(self, x):
         x = self.resnet18(x)
         return x
+    
+class resnet18_grayscale(nn.Module):
+    def __init__(self):
+        super(resnet18_grayscale, self).__init__()
+        self.resnet18 = models.resnet18(pretrained=True)
+        # change from supporting color to gray scale images
+        self.resnet18.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        n_inputs = self.resnet18.fc.in_features
+        self.resnet18.fc = nn.Linear(n_inputs, 136)
+                        
+    def forward(self, x):
+        x = self.resnet18(x)
+        return x
