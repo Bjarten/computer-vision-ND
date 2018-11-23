c28201285f98ad73f736b98c6b069cbc4b003152
Bjarten <bms_mok@hotmail.com>
2018-11-23 14:22:45 +0100

    feat(p1): implement Rotate transform

--- project_1_facial_keypoints/data_load.py
+++ project_1_facial_keypoints/data_load.py
@@ -422,6 +422,7 @@ class RandomHorizontalFlip(object):
         
         image_copy = np.copy(image)
         key_pts_copy = np.copy(key_pts)
+        key_pts_copy_2 = np.copy(key_pts_copy)
 
         if random.choice([0, 1]) <= 0.5:
             # horizontally flip image
@@ -517,4 +518,41 @@ class RandomHorizontalFlip(object):
 
             
         return {'image': image_copy, 'keypoints': key_pts_copy_2}
+
+# inspired by https://github.com/macbrennan90/facial-keypoint-detection/blob/master/CV_project.ipynb and
+# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_geometric_transformations/py_geometric_transformations.html
+class Rotate(object):
+    """Rotate image in sample by an angle"""
+    
+    def __init__(self, rotation):
+        self.rotation = rotation
+    
+    def __call__(self, sample):
+        image, key_pts = sample['image'], sample['keypoints']
+        
+        image_copy = np.copy(image)
+        key_pts_copy = np.copy(key_pts)
+        
+        rows = image.shape[0]
+        cols = image.shape[1]
+        
+        M = cv2.getRotationMatrix2D((rows/2,cols/2),90,1)
+        image_copy = cv2.warpAffine(image_copy,M,(cols,rows))
+                
+        
+        key_pts_copy = key_pts_copy.reshape((1,136))
+        new_keypoints = np.zeros(136)
+        
+        for i in range(68):
+            coord_idx = 2*i
+            old_coord = key_pts_copy[0][coord_idx:coord_idx+2]
+            new_coord = np.matmul(M,np.append(old_coord,1))
+            new_keypoints[coord_idx] += new_coord[0]
+            new_keypoints[coord_idx+1] += new_coord[1]
+        
+        new_keypoints = new_keypoints.reshape((68,2))
+        
+        return {'image': image_copy, 'keypoints': new_keypoints}
+    
+
     
