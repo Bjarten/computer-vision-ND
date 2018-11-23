60b2767f3b808ce047cef539247df89b4366de03
Bjarten <bms_mok@hotmail.com>
2018-11-23 08:36:54 +0100

    feat(p3): implement sense function for robot class in robot_class.py

--- project_3_landmark_detection/robot_class.py
+++ project_3_landmark_detection/robot_class.py
@@ -65,7 +65,7 @@ class robot:
     #        landmarks to be visible at all times
     #
     
-    ## TODO: paste your complete the sense function, here
+    ## paste your complete the sense function, here
     ## make sure the indentation of the code is correct
     def sense(self):
         ''' This function does not take in any parameters, instead it references internal variables
@@ -77,22 +77,29 @@ class robot:
             One item in the returned list should be in the form: [landmark_index, dx, dy].
             '''
            
-        measurements = None
+        measurements = []
         
-        ## TODO: iterate through all of the landmarks in a world
+        ## iterate through all of the landmarks in a world
         
-        ## TODO: For each landmark
+        ## For each landmark
         ## 1. compute dx and dy, the distances between the robot and the landmark
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
         ##    - Feel free to use the function self.rand() to help calculate this noise component
+        ##    - It may help to reference the `move` function for noise calculation
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
         
-        ## TODO: return the final, complete list of measurements
+        for i, landmark in enumerate(self.landmarks):
+            dx = landmark[0] - self.x + self.rand() * self.measurement_noise
+            dy = landmark[1] - self.y + self.rand() * self.measurement_noise
+            # check if measurment is in range
+            if abs(dx) <= self.measurement_range and abs(dy) <= self.measurement_range:
+                measurements.append([i,dx,dy])
+       
+        ## return the final, complete list of measurements
         return measurements
-
 
     # --------
     # make_landmarks:
